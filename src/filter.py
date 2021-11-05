6858378496b40b45ae03a4d4eb2096967db85867
Lorna <lorna.watson@epiphany.co.uk>
2021-11-05 12:42:05 +0000

    single JSON file that exports all nsfw results
    Co-authored-by: Daniaal Nadir <daniaal.nadir@smoothwall.com>

--- src/filter.py
+++ src/filter.py
@@ -5,6 +5,7 @@ import numpy as np
 import onnxruntime
 from utils import preprocess_image
 import json
+import time
 
 
 def dummy(x):
@@ -23,9 +24,14 @@ FILE_URLS = {
 }
 
 
+class Object:
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 class Filter:
     detection_model = None
     classes = None
+    me = Object()
 
     def __init__(self, model_name="default"):
         """
@@ -62,13 +68,15 @@ class Filter:
         image, scale = preprocess_image(img_path)
         if not min_prob:
             min_prob = 0.6
-
+ 
+        results = []
+ 
         outputs = self.detection_model.run(
             [s_i.name for s_i in self.detection_model.get_outputs()],
             {self.detection_model.get_inputs(
             )[0].name: np.expand_dims(image, axis=0)},
         )
-
+        
         labels = [op for op in outputs if op.dtype == "int32"][0]
         scores = [op for op in outputs if isinstance(op[0][0], np.float32)][0]
         boxes = [op for op in outputs if isinstance(op[0][0], np.ndarray)][0]
@@ -82,55 +90,75 @@ class Filter:
 
             box = box.astype(int).tolist()
             label = self.classes[label]
-            processed_boxes.append(
-                {"box": [int(c) for c in box], "score": float(score), "label": label})
-
-        img_name = os.path.splitext(os.path.basename(img_path))[0]
-
-        with open("/Users/lornn/Desktop/Nudity Filter/Results/flagged_"+img_name+".json", "w", encoding="utf-8") as f:
-            json.dump(processed_boxes, f, ensure_ascii=False, indent=4)
-
-        print("NSFW areas flagged: ((" + str(len(processed_boxes)) + ")) for image: "+os.path.basename(img_path))
+ 
+            processed_boxes.append({"box": [int(c) for c in box], "score": float(score), "label": label})
+             
+            result = Object()
+            result.box = [int(c) for c in box]
+            result.score = float(score)
+            result.label = label
+            results.append(result) 
+              
+        print("# Censored image")
+ 
+        finalObject = Object()
+        finalObject.imageOriginalUrl = img_path
+        finalObject.imageBlurredUrl = "blurred_" + img_path
+        finalObject.results = results
+        self.me.items.append(finalObject)
 
         return processed_boxes
 
     def censor_folder(self, folder_path, out_folder_path, parts_to_blur=[]):
+        self.me.items = []
         for img_filename in os.listdir(folder_path):
             path = os.path.join(folder_path, img_filename)
-            
             img = cv2.imread(path)
- 
-            if img is not None:
-                image = cv2.imread(path)
 
-                if image is not None:
-                    boxes = self.detect(path)
- 
-                    if len(boxes) > 0: 
-                        if parts_to_blur:
-                            boxes = [i["box"]
-                                    for i in boxes if i["label"] in parts_to_blur]
-                        else:
-                            boxes = [i["box"] for i in boxes]
+            if img is not None: 
+                boxes = self.detect(path)
 
-                        for box in boxes:
-                            x, y = box[0], box[1]
-                            w, h = box[2] - box[0], box[3] - box[1]
-
-                            ROI = image[y:y+h, x:x+w]
-                            blur = cv2.blur(ROI, (51, 51), 0)
-
-                            image[y:y+h, x:x+w] = blur
-
-                        if out_folder_path:
-                            fp = out_folder_path + "blurred_" + img_filename
-                            cv2.imwrite(fp, image)
+                if len(boxes) > 0:
+                    if parts_to_blur:
+                        boxes = [i["box"] for i in boxes if i["label"] in parts_to_blur]
                     else:
-                        print("# Ignored censorsing image " + img_filename)
+                        boxes = [i["box"] for i in boxes]
+
+                    for box in boxes:
+                        x, y = box[0], box[1]
+                        w, h = box[2] - box[0], box[3] - box[1]
+
+                        ROI = img[y:y+h, x:x+w]
+                        blur = cv2.blur(ROI, (51, 51), 0)
+
+                        img[y:y+h, x:x+w] = blur
+
+                    if out_folder_path:
+                        fp = out_folder_path + "blurred_" + img_filename
+                        cv2.imwrite(fp, img)
+            else:
+                print("# Ignored image")
+
+    def write_to_json(self):
+        print("# Write to json")
+ 
+        f = open("/Users/lornn/Desktop/Nudity Filter/Results/results.json", "w")
+        f.write(self.me.toJSON())
 
 
 if __name__ == "__main__":
+
+    start = time.time()
+    
+
     f = Filter()
 
     # Scan each image in X folder to see if needs NSFW censoring (i.e. blurring) and export censored version into a results folder. For each image we also generate an exported classes JSON file
-    f.censor_folder("/Users/lornn/Desktop/Nudity Filter/Test Images/", "/Users/lornn/Desktop/Nudity Filter/Results/")
+    f.censor_folder("/Users/lornn/Desktop/Nudity Filter/Test Images/",
+                    "/Users/lornn/Desktop/Nudity Filter/Results/")
+
+    f.write_to_json()
+
+    end = time.time()
+
+    print(end-start)
